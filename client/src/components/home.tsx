080fa333c9477dbc9ab526f3a68f7d81cc97a219
Bibilna Babu <bibilnababu@yahoo.com>
2023-04-03 11:56:16 +0100

    feat: added home page

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,3 +1,11 @@
-const Home: React.FC = () => <>Home!</>;
+const Home: React.FC = () => <>
+<p>
+    Welcome to the home of the justice department of Fakelandia
+</p>
+
+<p>
+    Here you can browse a list of recent Misdemeanours commited by our citizens,or you can confess to your own Misdemeanour.
+</p>
+</>;
 
 export default Home;
--- client/src/components/misdemeanour.tsx
+++ client/src/components/misdemeanour.tsx
@@ -1,3 +1,43 @@
-const Misdemeanour: React.FC = () => <>mmm</>;
+import React, { useContext, useEffect, useState } from 'react';
+
+const Misdemeanour = () => {
+ 
+  const [misdemeanours, setMisdemeanours] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8080/api/misdemeanours/100');
+      const data = await response.json();
+      setMisdemeanours(data);
+    };
+
+    fetchData();
+  }, [setMisdemeanours]);
+
+  
+  
+
+  return (
+    <div>
+     
+      <h1>Misdemeanours</h1>
+      <label htmlFor="filter">Filter by:</label>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>Citizen ID</th>
+            <th>Date</th>
+            <th>Misdemeanour</th>
+            <th>Punishment Idea</th>
+          </tr>
+        </thead>
+        
+      </table>
+    </div>
+  );
+};
+
 
 export default Misdemeanour;
+
